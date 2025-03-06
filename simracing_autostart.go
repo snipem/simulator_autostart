af707d0d9e6f49bf7afd6ba54bb880056878a13c
Matthias Küch <post@matthias-kuech.de>
2025-03-06 19:58:52 +0100

    Add iRacing

--- simracing_autostart.go
+++ simracing_autostart.go
@@ -10,8 +10,6 @@ import (
 	"github.com/mitchellh/go-ps"
 )
 
-var autostartedProcessIds []int
-
 func getProcessIdForExecutable(processName string) int {
 	processes, _ := ps.Processes()
 
@@ -50,25 +48,47 @@ func contains(ids []int, id int) bool {
 	return false
 }
 
+type State struct {
+	autostartedProcessIds []int
+}
+
 func main() {
 	log.Println("simracing_autostart started")
+	s := &State{}
 	for {
 		processName := "AMS2AVX.exe"
-		processId := getProcessIdForExecutable(processName)
-		if processId != -1 && !contains(autostartedProcessIds, processId) {
-			log.Printf("Autostarting processes for %s ...\n", processName)
-			err := startProcesses(
-				"C:\\Program Files (x86)\\Britton IT Ltd\\CrewChiefV4\\CrewChiefV4.exe",
-				"C:\\Program Files (x86)\\SimHub\\SimHubWPF.exe",
-				"C:\\Users\\mail\\AppData\\Local\\popometer\\popometer-recorder.exe",
-				"C:\\Users\\mail\\work\\sim-to-motec\\ams2-cli.bat",
-			)
-			if err == nil {
-				autostartedProcessIds = append(autostartedProcessIds, processId)
-			} else {
-				log.Println("Error:", err)
-			}
+		programsToStart := []string{
+			"C:\\Program Files (x86)\\Britton IT Ltd\\CrewChiefV4\\CrewChiefV4.exe",
+			"C:\\Program Files (x86)\\SimHub\\SimHubWPF.exe",
+			"C:\\Users\\mail\\AppData\\Local\\popometer\\popometer-recorder.exe",
+			"C:\\Users\\mail\\work\\sim-to-motec\\ams2-cli.bat"}
+
+		s.startProgramsIfProcessIsRunning(processName, programsToStart)
+
+		processName = "iRacingUI.exe"
+		programsToStart = []string{
+			"C:\\Program Files (x86)\\Britton IT Ltd\\CrewChiefV4\\CrewChiefV4.exe",
+			"C:\\Program Files (x86)\\SimHub\\SimHubWPF.exe",
+			"C:\\Users\\mail\\AppData\\Roaming\\garage61-install\\garage61-launcher.exe",
+			"C:\\Users\\mail\\AppData\\Local\\racelabapps\\RacelabApps.exe",
 		}
+
+		s.startProgramsIfProcessIsRunning(processName, programsToStart)
+
 		time.Sleep(5 * time.Second)
+	}
+}
+
+func (s *State) startProgramsIfProcessIsRunning(processName string, programsToStart []string) {
+	processId := getProcessIdForExecutable(processName)
+	if processId != -1 && !contains(s.autostartedProcessIds, processId) {
+		log.Printf("Autostarting processes for %s ...\n", processName)
+		err := startProcesses(
+			programsToStart...)
+		if err == nil {
+			s.autostartedProcessIds = append(s.autostartedProcessIds, processId)
+		} else {
+			log.Println("Error:", err)
+		}
 	}
 }
